4856cc7b49fc40e2214345ad2e2e35d7e05924b2
NPi2Loup <npiedeloup@gmail.com>
2015-09-04 13:36:21 +0200

    [Notification] Fixed notification's tests

---
+++ vertigo-x-notification/src/test/java/io/vertigo/x/notification/NotificationFeatures.java
@@ -0,0 +1,35 @@
+package io.vertigo.x.notification;
+
+import io.vertigo.core.config.Features;
+import io.vertigo.x.impl.notification.NotificationManagerImpl;
+import io.vertigo.x.plugins.notification.redis.RedisNotificationPlugin;
+
+/**
+ * Defines extension notification.
+ * @author pchretien
+ */
+public final class NotificationFeatures extends Features {
+
+	/**
+	 * Constructor.
+	 */
+	public NotificationFeatures() {
+		super("x-notification");
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	protected void setUp() {
+		getModuleConfigBuilder()
+				.addComponent(NotificationManager.class, NotificationManagerImpl.class);
+	}
+
+	/**
+	 * @return Active redis plugin
+	 */
+	public NotificationFeatures withRedis() {
+		getModuleConfigBuilder()
+				.addPlugin(RedisNotificationPlugin.class);
+		return this;
+	}
+}
