f923b8ea1d11a236bc65e716beac6a42be8daa3d
NPi2Loup <npiedeloup@gmail.com>
2015-09-04 13:31:05 +0200

    [Comment] Fixed comment's tests

---
+++ vertigo-x-comment/src/main/java/io/vertigo/x/impl/comment/CommentFeatures.java
@@ -0,0 +1,31 @@
+package io.vertigo.x.impl.comment;
+
+import io.vertigo.core.config.Features;
+import io.vertigo.x.comment.CommentManager;
+import io.vertigo.x.plugins.comment.redis.RedisCommentPlugin;
+
+/**
+ * Defines extension account.
+ * @author pchretien
+ */
+public final class CommentFeatures extends Features {
+
+	public CommentFeatures() {
+		super("x-comment");
+	}
+
+	@Override
+	protected void setUp() {
+		getModuleConfigBuilder()
+				.addComponent(CommentManager.class, CommentManagerImpl.class);
+	}
+
+	/**
+	 * @return Active redis plugin
+	 */
+	public CommentFeatures withRedis() {
+		getModuleConfigBuilder()
+				.addPlugin(RedisCommentPlugin.class);
+		return this;
+	}
+}
--- vertigo-x-comment/src/test/java/io/vertigo/x/comment/MyAppConfig.java
+++ vertigo-x-comment/src/test/java/io/vertigo/x/comment/MyAppConfig.java
@@ -1,7 +1,6 @@
 package io.vertigo.x.comment;
 
-import io.vertigo.commons.codec.CodecManager;
-import io.vertigo.commons.impl.codec.CodecManagerImpl;
+import io.vertigo.commons.impl.CommonsFeatures;
 import io.vertigo.commons.plugins.resource.java.ClassPathResourceResolverPlugin;
 import io.vertigo.core.config.AppConfig;
 import io.vertigo.core.config.AppConfigBuilder;
@@ -11,41 +10,23 @@ import io.vertigo.core.impl.locale.LocaleManagerImpl;
 import io.vertigo.core.impl.resource.ResourceManagerImpl;
 import io.vertigo.core.locale.LocaleManager;
 import io.vertigo.core.resource.ResourceManager;
-import io.vertigo.dynamo.file.FileManager;
-import io.vertigo.dynamo.impl.file.FileManagerImpl;
+import io.vertigo.dynamo.impl.DynamoFeatures;
 import io.vertigo.dynamo.plugins.environment.loaders.java.AnnotationLoaderPlugin;
 import io.vertigo.dynamo.plugins.environment.registries.domain.DomainDynamicRegistryPlugin;
-import io.vertigo.persona.impl.security.VSecurityManagerImpl;
-import io.vertigo.persona.security.VSecurityManager;
-import io.vertigo.vega.impl.rest.RestManagerImpl;
-import io.vertigo.vega.plugins.rest.handler.ExceptionRestHandlerPlugin;
-import io.vertigo.vega.plugins.rest.handler.JsonConverterRestHandlerPlugin;
-import io.vertigo.vega.plugins.rest.handler.RestfulServiceRestHandlerPlugin;
-import io.vertigo.vega.plugins.rest.handler.SecurityRestHandlerPlugin;
-import io.vertigo.vega.plugins.rest.handler.SessionInvalidateRestHandlerPlugin;
-import io.vertigo.vega.plugins.rest.handler.SessionRestHandlerPlugin;
-import io.vertigo.vega.plugins.rest.handler.ValidatorRestHandlerPlugin;
-import io.vertigo.vega.plugins.rest.instrospector.annotations.AnnotationsEndPointIntrospectorPlugin;
-import io.vertigo.vega.plugins.rest.routesregister.sparkjava.SparkJavaRoutesRegisterPlugin;
-import io.vertigo.vega.rest.RestManager;
+import io.vertigo.persona.impl.security.PersonaFeatures;
+import io.vertigo.vega.VegaFeatures;
 import io.vertigo.vega.rest.WebServices;
-import io.vertigo.vega.rest.engine.GoogleJsonEngine;
-import io.vertigo.vega.rest.engine.JsonEngine;
-import io.vertigo.x.account.AccountManager;
 import io.vertigo.x.comment.data.TestUserSession;
-import io.vertigo.x.connectors.redis.RedisConnector;
-import io.vertigo.x.impl.account.AccountDefinitionProvider;
-import io.vertigo.x.impl.account.AccountManagerImpl;
-import io.vertigo.x.impl.comment.CommentManagerImpl;
-import io.vertigo.x.plugins.account.redis.RedisAccountStorePlugin;
-import io.vertigo.x.plugins.comment.redis.RedisCommentPlugin;
-import io.vertigo.x.webapi.account.AccountWebServices;
+import io.vertigo.x.connectors.ConnectorsFeatures;
+import io.vertigo.x.impl.account.AccountFeatures;
+import io.vertigo.x.impl.comment.CommentFeatures;
 import io.vertigo.x.webapi.comment.CommentWebServices;
 
 import java.io.IOException;
 import java.net.InetAddress;
 
 public final class MyAppConfig {
+	public static final int WS_PORT = 8088;
 
 	private static boolean ping(final String host) {
 		try {
@@ -57,17 +38,17 @@ public final class MyAppConfig {
 	}
 
 	private static AppConfigBuilder createAppConfigBuilder() {
-		final String host;
-		final int port;
-		final String password;
+		final String redisHost;
+		final int redisPort;
+		final String redisPassword;
 		if (ping("kasper-redis")) {
-			host = "kasper-redis";
-			port = 6379;
-			password = null;
+			redisHost = "kasper-redis";
+			redisPort = 6379;
+			redisPassword = null;
 		} else if (ping("pub-redis-10382.us-east-1-3.2.ec2.garantiadata.com")) {
-			host = "pub-redis-10382.us-east-1-3.2.ec2.garantiadata.com";
-			port = 10382;
-			password = "kleegroup";
+			redisHost = "pub-redis-10382.us-east-1-3.2.ec2.garantiadata.com";
+			redisPort = 10382;
+			redisPassword = "kleegroup";
 		} else {
 			throw new RuntimeException("no redis server found");
 		}
@@ -86,31 +67,12 @@ public final class MyAppConfig {
 			.beginBoot()
 				.silently()
 			.endBoot()
-			.beginModule("persona")
-				.beginComponent(VSecurityManager.class, VSecurityManagerImpl.class)
-					.addParam("userSessionClassName", TestUserSession.class.getName())
-				.endComponent()
-			.endModule()
-			.beginModule("dynamo").withNoAPI()
-				.addComponent(CodecManager.class, CodecManagerImpl.class)
-				.addComponent(FileManager.class, FileManagerImpl.class)
-			.endModule()
-			.beginModule("connector").withNoAPI()
-				.beginComponent(RedisConnector.class, RedisConnector.class)
-					.addParam("host", host)
-					.addParam("port", Integer.toString(port))
-					.addParam("password", password)
-				.endComponent()
-			.endModule()
-			.beginModule("account")
-				.addDefinitionProvider(AccountDefinitionProvider.class)
-				.addComponent(AccountManager.class, AccountManagerImpl.class)
-				.addPlugin(RedisAccountStorePlugin.class)
-			.endModule()
-			.beginModule("comment")
-				.addComponent(CommentManager.class, CommentManagerImpl.class)
-				.addPlugin(RedisCommentPlugin.class)
-			.endModule();
+			.beginModule(PersonaFeatures.class).withUserSession(TestUserSession.class).endModule()
+			.beginModule(DynamoFeatures.class).endModule()
+			.beginModule(CommonsFeatures.class).endModule()
+			.beginModule(ConnectorsFeatures.class).withRedis(redisHost, redisPort, redisPassword).endModule()
+			.beginModule(AccountFeatures.class).withRedis().endModule()
+			.beginModule(CommentFeatures.class).withRedis().endModule();
 		// @formatter:on
 	}
 
@@ -122,27 +84,13 @@ public final class MyAppConfig {
 	public static AppConfig vegaConfig() {
 		// @formatter:off
 		return createAppConfigBuilder()
-			.beginModule("restServices").withNoAPI().withInheritance(WebServices.class)
-				.addComponent(AccountWebServices.class)
+			.beginModule(VegaFeatures.class).withEmbeddedServer(WS_PORT).endModule()
+			.beginModule("ws-comment").withNoAPI().withInheritance(WebServices.class)
 				.addComponent(CommentWebServices.class)
 				.addComponent(TestLoginWebServices.class)
 			.endModule()
-			.beginModule("restCore").withNoAPI().withInheritance(Object.class)
-				.addComponent(JsonEngine.class, GoogleJsonEngine.class)
-				.addComponent(RestManager.class, RestManagerImpl.class)
-				.addPlugin(AnnotationsEndPointIntrospectorPlugin.class)
-				.addPlugin(SparkJavaRoutesRegisterPlugin.class)
-				//-- Handlers plugins
-				.addPlugin(ExceptionRestHandlerPlugin.class)
-				.addPlugin(SessionInvalidateRestHandlerPlugin.class)
-				.addPlugin(SessionRestHandlerPlugin.class)
-				.addPlugin(SecurityRestHandlerPlugin.class)
-				//.beginPlugin(OldJsonConverterRestHandlerPlugin.class)
-				.addPlugin(JsonConverterRestHandlerPlugin.class)
-				.addPlugin(ValidatorRestHandlerPlugin.class)
-				.addPlugin(RestfulServiceRestHandlerPlugin.class)
-			.endModule()
-		.build();
+			.build();
 		// @formatter:on
 	}
+
 }
