7269f6875eca16c3f302f4a356b5035c7f8b88de
phlppchrtn <pchretien55@gmail.com>
2015-10-22 15:48:31 +0200

    [core]removed  resourceManager and configManager from  config

--- vertigo-x-account/src/test/java/io/vertigo/x/account/MyAppConfig.java
+++ vertigo-x-account/src/test/java/io/vertigo/x/account/MyAppConfig.java
@@ -7,9 +7,7 @@ import io.vertigo.core.config.AppConfigBuilder;
 import io.vertigo.core.environment.EnvironmentManager;
 import io.vertigo.core.impl.environment.EnvironmentManagerImpl;
 import io.vertigo.core.impl.locale.LocaleManagerImpl;
-import io.vertigo.core.impl.resource.ResourceManagerImpl;
 import io.vertigo.core.locale.LocaleManager;
-import io.vertigo.core.resource.ResourceManager;
 import io.vertigo.dynamo.impl.DynamoFeatures;
 import io.vertigo.dynamo.plugins.environment.loaders.java.AnnotationLoaderPlugin;
 import io.vertigo.dynamo.plugins.environment.registries.domain.DomainDynamicRegistryPlugin;
@@ -54,11 +52,10 @@ public final class MyAppConfig {
 		// @formatter:off
 		return new AppConfigBuilder()
 			.beginBootModule()
+				.beginPlugin( ClassPathResourceResolverPlugin.class).endPlugin()
 				.beginComponent(LocaleManager.class, LocaleManagerImpl.class)
 					.addParam("locales", "fr")
 				.endComponent()
-				.addComponent(ResourceManager.class, ResourceManagerImpl.class)
-					.beginPlugin( ClassPathResourceResolverPlugin.class).endPlugin()
 				.addComponent(EnvironmentManager.class, EnvironmentManagerImpl.class)
 					.beginPlugin(AnnotationLoaderPlugin.class).endPlugin()
 					.beginPlugin(DomainDynamicRegistryPlugin.class).endPlugin()
