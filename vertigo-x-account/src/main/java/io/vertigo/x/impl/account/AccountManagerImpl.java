7b601560126abab7968c8b85d5b0dd820348abd8
phlppchrtn <pchretien55@gmail.com>
2016-01-20 18:03:48 +0100

    + simple

--- vertigo-x-account/src/main/java/io/vertigo/x/impl/account/AccountManagerImpl.java
+++ vertigo-x-account/src/main/java/io/vertigo/x/impl/account/AccountManagerImpl.java
@@ -2,10 +2,8 @@ package io.vertigo.x.impl.account;
 
 import io.vertigo.dynamo.domain.model.URI;
 import io.vertigo.dynamo.file.FileManager;
-import io.vertigo.dynamo.file.model.InputStreamBuilder;
 import io.vertigo.dynamo.file.model.VFile;
 import io.vertigo.lang.Assertion;
-import io.vertigo.lang.Option;
 import io.vertigo.lang.WrappedException;
 import io.vertigo.persona.security.UserSession;
 import io.vertigo.persona.security.VSecurityManager;
@@ -13,12 +11,9 @@ import io.vertigo.x.account.Account;
 import io.vertigo.x.account.AccountGroup;
 import io.vertigo.x.account.AccountManager;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
+import java.io.File;
+import java.net.URISyntaxException;
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -46,34 +41,16 @@ public final class AccountManagerImpl implements AccountManager {
 		Assertion.checkNotNull(securityManager);
 		//-----
 		this.accountStorePlugin = accountStorePlugin;
-		//TODO a remplacer par l'appel a fileManager en v0.9.1
-		defaultPhoto = createFile("defaultPhoto.png", "image/png", AccountManagerImpl.class.getResource("defaultPhoto.png"), fileManager);
 		this.securityManager = securityManager;
+		defaultPhoto = createDefaultPhoto(fileManager);
 	}
 
-	private static VFile createFile(final String fileName, final String typeMime, final URL ressourceUrl, final FileManager fileManager) {
-		final long length;
-		final long lastModified;
-		final URLConnection conn;
+	private static VFile createDefaultPhoto(final FileManager fileManager) {
 		try {
-			conn = ressourceUrl.openConnection();
-			try {
-				length = conn.getContentLength();
-				lastModified = conn.getLastModified();
-			} finally {
-				conn.getInputStream().close();
-			}
-		} catch (final IOException e) {
-			throw new WrappedException("Can't get file size", e);
+			return fileManager.createFile(new File(AccountManagerImpl.class.getResource("defaultPhoto.png").toURI()));
+		} catch (final URISyntaxException e) {
+			throw new WrappedException(e);
 		}
-		Assertion.checkArgument(length >= 0, "Can't get file size");
-		final InputStreamBuilder inputStreamBuilder = new InputStreamBuilder() {
-			@Override
-			public InputStream createInputStream() throws IOException {
-				return ressourceUrl.openStream();
-			}
-		};
-		return fileManager.createFile(fileName, typeMime, new Date(lastModified), length, inputStreamBuilder);
 	}
 
 	/** {@inheritDoc} */
@@ -167,8 +144,6 @@ public final class AccountManagerImpl implements AccountManager {
 	/** {@inheritDoc} */
 	@Override
 	public VFile getPhoto(final URI<Account> accountURI) {
-		final Option<VFile> photo = accountStorePlugin.getPhoto(accountURI);
-		return photo.getOrElse(defaultPhoto);
+		return accountStorePlugin.getPhoto(accountURI).getOrElse(defaultPhoto);
 	}
-
 }
