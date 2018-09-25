e557542e2c1c909492eb1ca61feb34d1eae61b54
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2018-09-25 17:19:06 +0200

    [ui] add a redirect

--- vertigo-ui/src/test/java/io/vertigo/ui/controller/MovieController.java
+++ vertigo-ui/src/test/java/io/vertigo/ui/controller/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import io.vertigo.ui.core.ViewContext;
 import io.vertigo.ui.core.ViewContextKey;
@@ -35,9 +36,11 @@ public class MovieController extends AbstractVSpringMvcController {
 	}
 
 	@PostMapping("/_save")
-	public void doSave(
-			@ViewAttribute("movie") final Movie movie) {
+	public String doSave(
+			@ViewAttribute("movie") final Movie movie, final RedirectAttributes redirectAttributes) {
 		movieServices.save(movie);
+		redirectAttributes.addAttribute("movId", movie.getMovId());
+		return "redirect:/movie/";
 	}
 
 }
