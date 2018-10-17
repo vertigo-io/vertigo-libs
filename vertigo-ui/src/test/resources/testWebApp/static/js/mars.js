6a6f4ec8aff0abddf2e8d49c2807ae2d05df059a
NPi2Loup <npiedeloup@gmail.com>
2018-10-17 16:21:40 +0200

    [Ui] Added missing MMC files

---
+++ vertigo-ui/src/test/resources/testWebApp/static/js/mars.js
@@ -0,0 +1,25 @@
+/******
+ * 
+ * @returns
+ */
+function configDatePickers(){
+        $(".dateSelector").datepicker({
+                                                todayBtn: "linked",
+                                                clearBtn: true,
+                                                language: "fr"
+        });
+}
+
+/**
+ *  CORRECTIF permettant de mofidier les datepickers avec l'autocompletion du navigateur
+ *  Prend en compte tous les inputs avec un id commençant par "datepicker"
+ */
+function patchDatePicker(){
+        let listeDatepicker = $('input[id^="datepicker"]');
+        listeDatepicker.each(function(idx){
+                let datepicker = $(this);               
+                datepicker.select(function(e){                  
+                        datepicker.datepicker("update", datepicker.val());      
+                });
+        });
+}
