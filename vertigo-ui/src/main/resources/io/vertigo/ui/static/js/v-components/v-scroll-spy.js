dae850ea5a027fd8171831bbd5fabfd360195d4a
NPi2Loup <npiedeloup@gmail.com>
2018-10-31 15:26:24 +0100

    [Ui] Doc

--- vertigo-ui/src/main/resources/io/vertigo/ui/static/js/v-components/v-scroll-spy.js
+++ vertigo-ui/src/main/resources/io/vertigo/ui/static/js/v-components/v-scroll-spy.js
@@ -32,50 +32,15 @@ Vue.directive('scroll-spy', {
         		var scrollBreakpoints = Vue.computeBreakPoints(scrollPosition);
         		//We looks between which breakpoints we are
         		for(var i = 0 ; i < elAs.length; i++) {
-        			//console.log(i+' '+' '+scrollPosition+' '+scrollBreakpoints[i] )
-				    if(scrollBreakpoints[i] <= scrollPosition && (i >= elAs.length-1 || scrollPosition < scrollBreakpoints[i+1])) {
+        			if(scrollBreakpoints[i] <= scrollPosition && (i >= elAs.length-1 || scrollPosition < scrollBreakpoints[i+1])) {
 				      elAs[i].classList.add("active")
 				  } else {
 					  elAs[i].classList.remove("active");
 				  }
-			      //elAs[i].addEventListener('scroll', Vue.scrollSpyHandler)
 			    }
         	};
         	
-        	Vue.computeBreakPoints = function(scrollPosition){
-        		var scrollBreakpoints = []
-        		scrollBreakpoints.push(0)
-        		for(var i = 1 ; i < elAs.length; i++) {
-        			const elScrollId = elAs[i].hash;
-				    const elScroll = document.querySelector(elScrollId)
-				    if(elScroll) {
-				    	scrollBreakpoints.push(scrollPosition+elScroll.getBoundingClientRect().top-offset);      
-					} else {
-						console.warn('ScrollSpy element '+elScrollId+' not found')
-					}
-			    }
-        		
-        		const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-        		const scrollHeight = Quasar.utils.scroll.getScrollHeight(scrollContainer)
-        		const scrollMax = scrollHeight - windowHeight
-        		const lastHeight = Quasar.utils.scroll.getScrollHeight(scrollContainer) - scrollBreakpoints[scrollBreakpoints.length-1];
-        		const scrollStart = scrollMax - lastHeight + offset
-        		//console.log('scrollMax '+scrollMax+'  lastHeight '+lastHeight+'  scrollStart '+scrollStart )
-        		
-        		for(var i = 1 ; i < elAs.length; i++) {
-        			var prev = scrollBreakpoints[i];
-        			if(scrollBreakpoints[i] > scrollStart) {
-        				scrollBreakpoints[i] = scrollStart + ((scrollBreakpoints[i] - scrollStart)/lastHeight) * (scrollMax-scrollStart-offset)
-        			}	
-        			scrollBreakpoints[i] = Math.round(scrollBreakpoints[i])
-        			//console.log(i+'  from: '+prev+ '  to:'+scrollBreakpoints[i] )
-        		}        		
-        		
-        		
-        		return scrollBreakpoints;
-            };
-            
-            Vue.scrollTo = function(event){
+        	Vue.scrollTo = function(event){
         		event.preventDefault();
         		const elScrollId = event.target.hash;
         		const elScroll = document.querySelector(elScrollId)
@@ -90,14 +55,42 @@ Vue.directive('scroll-spy', {
         			}
 			    }
                 var duration = 200
-                //console.log(Quasar.utils.scroll.getScrollPosition(scrollContainer) +' to:'+toScroll+'  top:'+elScroll.getBoundingClientRect().top)
                 Quasar.utils.scroll.setScrollPosition(scrollContainer,toScroll, duration)
+            };
+        	
+        	Vue.computeBreakPoints = function(scrollPosition){
+        		var scrollBreakpoints = []
+        		scrollBreakpoints.push(0)
+        		for(var i = 1 ; i < elAs.length; i++) {
+        			const elScrollId = elAs[i].hash;
+				    const elScroll = document.querySelector(elScrollId)
+				    if(elScroll) {
+				    	scrollBreakpoints.push(scrollPosition+elScroll.getBoundingClientRect().top-offset);      
+					} else {
+						console.warn('ScrollSpy element '+elScrollId+' not found')
+					}
+			    }
+        		
+        		const windowHeight = (window.innerHeight || document.documentElement.clientHeight); /** visible height */
+        		const scrollHeight = Quasar.utils.scroll.getScrollHeight(scrollContainer) /** height of scrollable element */ 
+        		const scrollMax = scrollHeight - windowHeight /** Maximum possible scroll */  
+        		const lastHeight = Quasar.utils.scroll.getScrollHeight(scrollContainer) - scrollBreakpoints[scrollBreakpoints.length-1]; /** Last element height */
+        		const scrollStart = scrollMax - lastHeight + offset /** Start linear move at this scroll position */
+        		
+        		for(var i = 1 ; i < elAs.length; i++) {
+        			var prev = scrollBreakpoints[i];
+        			if(scrollBreakpoints[i] > scrollStart) {
+        				scrollBreakpoints[i] = scrollStart + ((scrollBreakpoints[i] - scrollStart)/lastHeight) * (scrollMax-scrollStart-offset)
+        			}	
+        			scrollBreakpoints[i] = Math.round(scrollBreakpoints[i])
+        			//console.log(i+'  from: '+prev+ '  to:'+scrollBreakpoints[i] )
+        		}
+        		return scrollBreakpoints;
             };
             
             for(var i = 0 ; i < elAs.length; i++) {
     			elAs[i].addEventListener('click', Vue.scrollTo);
-    		}
-		    
+    		}		    
         	window.addEventListener('scroll', Vue.scrollSpyHandler)
         },
         unbind: function(el) {
