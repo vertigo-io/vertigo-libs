cd007e3fe6b42b315be12a8abbacbf030d47919f
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2018-11-15 18:21:38 +0100

    [ui] show more on facets

--- vertigo-ui/src/main/resources/io/vertigo/ui/static/js/v-components/v-facets.js
+++ vertigo-ui/src/main/resources/io/vertigo/ui/static/js/v-components/v-facets.js
@@ -4,22 +4,33 @@ Vue.component('v-facets', {
  + '	<div v-for="(selectedFacetValues, selectedFacet) in selectedFacets" :key="selectedFacet">'
  + '		<q-chip class="q-mb-sm" v-for="selectedFacetValue in selectedFacetValues" :key="selectedFacetValue.code" @click="$emit(\'toogle-facet\', selectedFacet, selectedFacetValue, contextKey)" icon-right="cancel"><big>{{facetLabelByCode(selectedFacet)}}: {{facetValueLabelByCode(selectedFacet, selectedFacetValue)}}</big></q-chip>'
  + '	</div>'
- + '</div>'
- + '<q-list v-if="facet.multiple || !isFacetSelected(facet.code)" v-for="facet in facets" :key="facet.code" class="facetValues" dense highlight no-border>'
- + '<q-list-header><big>{{facet.label}}</big></q-list-header>'
- + '	  		<q-item v-for="value in facet.values" :key="value.code" class="facetValue q-ml-md">'
- + '	  			<q-checkbox v-if="facet.multiple" v-bind:value="isFacetValueSelected(facet.code, value.code)" :label="facetValueLabel(value.label, value.count)" @change="$emit(\'toogle-facet\', facet.code, value.code, contextKey)" ></q-checkbox>'
- + '	  			<span v-else @click="$emit(\'toogle-facet\', facet.code, value.code, contextKey)" >{{facetValueLabel(value.label, value.count)}}</span>'
+ + '	</div>'
+ + '	<q-list v-if="facet.multiple || !isFacetSelected(facet.code)" v-for="facet in facets" :key="facet.code" class="facetValues" dense highlight no-border>'
+ + '		<q-list-header><big>{{facet.label}}</big></q-list-header>'
+ + '	  		<q-item v-for="(value, index) in visibleFacets(facet.code, facet.values)" :key="value.code" class="facetValue q-ml-md">'
+ +'						<q-checkbox v-if="facet.multiple" v-bind:value="isFacetValueSelected(facet.code, value.code)" :label="facetValueLabel(value.label, value.count)" @change="$emit(\'toogle-facet\', facet.code, value.code, contextKey)" ></q-checkbox>'
+ + '	  				<span v-else @click="$emit(\'toogle-facet\', facet.code, value.code, contextKey)" >{{facetValueLabel(value.label, value.count)}}</span>'
  + '	  		</q-item>'
- + '	  </q-list>'
+ + '			<span v-if="facet.values.length > maxValues && !isFacetExpanded(facet.code)" @click="expandFacet(facet.code)">Show All</span>'
+ + '			<span v-if="facet.values.length > maxValues && isFacetExpanded(facet.code)" @click="reduceFacet(facet.code)">Show Less</span>'
+ + '	</q-list>'
  + '</div>'
   ,
   props: {
     facets: Array,
     selectedFacets: Object,
-    contextKey: String
+    contextKey: String,
+    maxValues: {
+    	type : Number,
+    	default : 5
+    }
   },
   computed: {
+  },
+  data : function() {
+	  return {
+		  expandedFacets:[]
+	  }
   },
   methods: {
 	  facetByCode : function (facetCode) {
@@ -43,6 +54,25 @@ Vue.component('v-facets', {
 	  },
 	  facetValueLabel : function (label, count){
 		  return label + ' (' + count + ')';
+	  },
+	  expandFacet : function (facetCode){
+		  if (!this.isFacetExpanded(facetCode)) {
+			  this.$data.expandedFacets.push(facetCode);
+		  }
+	  },
+	  reduceFacet : function (facetCode){
+		  if (this.isFacetExpanded(facetCode)) {
+			  this.$data.expandedFacets.splice( this.$data.expandedFacets.indexOf(facetCode), 1);
+		  }
+	  },
+	  isFacetExpanded : function (facetCode){
+		 return this.$data.expandedFacets.includes(facetCode);
+	  },
+	  visibleFacets : function (facetCode, facetValues){
+		  if (!this.isFacetExpanded(facetCode)) {
+			  return  facetValues.slice(0, this.maxValues);
+		  }
+		  return facetValues;
 	  }
   }
 })
