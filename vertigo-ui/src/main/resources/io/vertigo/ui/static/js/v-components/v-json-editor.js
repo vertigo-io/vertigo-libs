51653c641c822cfe13ba9c3beb0002a4042b9843
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2018-12-10 18:09:44 +0100

    [ui] added a basic json editor component

---
+++ vertigo-ui/src/main/resources/io/vertigo/ui/static/js/v-components/v-json-editor.js
@@ -0,0 +1,27 @@
+Vue.component('v-json-editor', {
+	template : 
+'	<div class="row">'
++'		<div v-for="(value, key) in jsonAsObject" :key="key" :class="\'col-\'+(12/cols)">'
++'			<q-field :label="key" orientation="vertical">'
++'				<q-input v-if="!readonly"  v-model="jsonAsObject[key]" @input="updateJson" ></q-input>'
++'				<span v-else>{{value}}</span>'					
++'			</q-field>'
++'		</div>'
++'	</div>'
+	,
+	props : {
+		value: { type: String, required: true},
+		readonly : { type: Boolean, required: true },
+		cols : { type: Number, 'default': 2 }
+	},
+	data : function () {
+		return {
+			jsonAsObject : JSON.parse(this.$props.value)
+		}
+	},
+	methods: {
+	    updateJson() {
+	      this.$emit('input', JSON.stringify(this.$data.jsonAsObject));
+	    }
+	  }
+})
