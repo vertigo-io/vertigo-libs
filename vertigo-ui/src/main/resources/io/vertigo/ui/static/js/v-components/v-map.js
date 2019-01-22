9f4d6e6650ef6516bf0ebd4a99bf8eaac7cdc6d2
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2019-01-22 22:15:16 +0100

    [ui] map add popup support

--- vertigo-ui/src/main/resources/io/vertigo/ui/static/js/v-components/v-map.js
+++ vertigo-ui/src/main/resources/io/vertigo/ui/static/js/v-components/v-map.js
@@ -1,13 +1,25 @@
 Vue.component('v-map', {
-	template : '<div :id="id" ></div>',
+	template : '<div :id="id" >'
++'					<q-popover v-model="popupDisplayed" id="popup" self="top middle" class="q-pa-md">'
++'						{{popupTitle}}'
++'					</q-popover>'
++'				</div>'
+,
 	props : {
 		id: { type: String, required: true},
 		list : { type: Array, required: true },
 		field: { type: String, required: true},
+		nameField: { type: String, required: true},
 		markerColor : { type: String, 'default': "#000000" },
 		markerFont : { type: String, 'default': "Material Icons" },
 		markerIcon : { type: String, 'default': "place" },
 		markerSize : { type: String, 'default': "45px" }
+	},
+	data : function () {
+		return {
+			popupDisplayed : false,
+			popupTitle: ''
+		}
 	},
 	mounted : function() {
 		var view = new ol.View();
@@ -21,6 +33,7 @@ Vue.component('v-map', {
 		});
 
 		var geoField = this.$props.field;
+		var nameField = this.$props.nameField;
 		features = this.$props.list.map(function(object) {
 			var geoObject;
 			if (typeof object[geoField] === 'string' || object[geoField] instanceof String){
@@ -31,6 +44,8 @@ Vue.component('v-map', {
 			var iconFeature = new ol.Feature({
 				geometry : new ol.geom.Point(ol.proj.fromLonLat([ geoObject.lon, geoObject.lat ])),
 			});
+			
+			iconFeature.set('name', object[nameField]);
 			
 			iconFeature.setStyle(styleIcon);
 			return iconFeature
@@ -59,7 +74,42 @@ Vue.component('v-map', {
 			view : view
 		});
 		
+		// fit view
 		map.getView().fit(vectorLayer.getSource().getExtent(), map.getSize());
+		
+		var popup = new ol.Overlay({
+	        element: this.$el.querySelector('#popup'),
+	        positioning: 'bottom-center',
+	        stopEvent: false,
+	        offset: [0, -50]
+	      });
+	    map.addOverlay(popup);
+		// display popup on click
+        map.on('click', function(evt) {
+          var feature = map.forEachFeatureAtPixel(evt.pixel,
+            function(feature) {
+              return feature;
+            });
+          if (feature) {
+            var coordinates = feature.getGeometry().getCoordinates();
+            popup.setPosition(coordinates);
+            this.$data.popupDisplayed = true;
+            this.$data.popupTitle = feature.get('name');
+          } else {
+          	this.$data.popupDisplayed = false;
+          }
+        }.bind(this));
+	      
+	    // change mouse cursor when over marker
+	    map.on('pointermove', function(e) {
+			if (e.dragging) {
+				this.$data.popupDisplayed = false;
+			  return;
+			}
+			var pixel = map.getEventPixel(e.originalEvent);
+			var hit = map.hasFeatureAtPixel(pixel);
+			map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+		}.bind(this));
 		
 
 	}
