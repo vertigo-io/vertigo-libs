98c2bb400fb516bc1f4573ec60ff7cff543e1a5c
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2019-09-05 14:43:30 +0200

    [ui] +simple

--- vertigo-ui/src/main/resources/io/vertigo/ui/static/js/vertigo-ui.js
+++ vertigo-ui/src/main/resources/io/vertigo/ui/static/js/vertigo-ui.js
@@ -272,7 +272,7 @@ var VUi = {
 						return '';
 					}
 				},
-				prepareParamsFromVueData: function (...keys) {
+				vueDataParams: function (...keys) {
 					var params = {};
 					for (var i = 0; i < keys.length; i++) {
 						var contextKey = keys[i];
