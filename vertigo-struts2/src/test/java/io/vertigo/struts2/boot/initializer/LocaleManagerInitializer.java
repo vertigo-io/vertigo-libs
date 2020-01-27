961845c337017498ba111a594fefd76241b264a0
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2020-01-27 11:10:03 +0100

    [all] fix resources

--- vertigo-struts2/src/test/java/io/vertigo/struts2/boot/initializer/LocaleManagerInitializer.java
+++ vertigo-struts2/src/test/java/io/vertigo/struts2/boot/initializer/LocaleManagerInitializer.java
@@ -36,8 +36,8 @@ public final class LocaleManagerInitializer implements ComponentInitializer {
 	/** {@inheritDoc} */
 	@Override
 	public void init() {
-		localeManager.add("io.vertigo.dynamox.domain.constraint.Constraint", io.vertigo.datamodel.impl.smarttype.constraint.Resources.values());
-		localeManager.add("io.vertigo.dynamox.domain.formatter.Formatter", io.vertigo.datamodel.impl.smarttype.formatter.Resources.values());
+		localeManager.add("io.vertigo.datamodel.impl.smarttype.constraint.Constraint", io.vertigo.datamodel.impl.smarttype.constraint.Resources.values());
+		localeManager.add("io.vertigo.datamodel.impl.smarttype.formatter.Formatter", io.vertigo.datamodel.impl.smarttype.formatter.Resources.values());
 
 		// Messages Ui vertigo
 		localeManager.add("io.vertigo.struts2.resources.Resources", io.vertigo.struts2.resources.Resources.values());
