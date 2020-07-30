ea515204b31033811609b14e1d365c8509cdad52
Piedeloup Nicolas <nicolas.piedeloup@kleegroup.com>
2020-07-30 14:54:07 +0200

    [Ui] Changed npm serve port to 3000

--- vertigo-ui/src/vuejs-lib/vertigo-ui/vue.config.js
+++ vertigo-ui/src/vuejs-lib/vertigo-ui/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     configureWebpack: {
+    	output: {
+    	  library: 'VertigoUi', // Add this line to expose the library in the devServer
+    	  libraryTarget: 'umd'
+    	},
+	    devServer: {
+	      headers: { 'Access-Control-Allow-Origin': '*' }
+	    },
         externals: {
             quasar: {
                 commonjs: 'quasar',
