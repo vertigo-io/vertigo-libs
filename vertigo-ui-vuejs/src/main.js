9deff1da970fdce2fc8d4a7385dfa2f9ae153858
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2020-09-03 11:22:01 +0200

    [ui-vuejs] include library to main repo

---
+++ vertigo-ui-vuejs/src/main.js
@@ -0,0 +1,61 @@
+import Quasar from 'quasar'
+import VChatbot from './components/VChatbot.vue'
+import VCommands from './components/VCommands.vue'
+import VComments from './components/VComments.vue'
+import VExtensionsStore from './components/VExtensionsStore.vue'
+import VFacets from './components/VFacets.vue'
+import VGeopointInput from './components/VGeopointInput.vue'
+import VHandles from './components/VHandles.vue'
+import VJsonEditor from './components/VJsonEditor.vue'
+import VNotifications from './components/VNotifications.vue'
+import VMap from './components/VMap.vue'
+
+
+import VMinify from './directives/VMinify'
+import VScrollSpy from './directives/VScrollSpy'
+
+import VMethods from './methods'
+
+import EnUs from './lang/vertigo-ui-lang-en-us'
+import Fr from './lang/vertigo-ui-lang-fr'
+
+export function getBoundMethods(obj, methods) {
+    let boundMethods = {};
+    Object.keys(methods)
+        .forEach(methodName => boundMethods[methodName] = methods[methodName].bind(obj));
+    return boundMethods;
+}
+
+export function install (Vue , /*options*/) {
+      
+    // components
+    Vue.component("v-chatbot", VChatbot);
+    Vue.component("v-commands", VCommands);
+    Vue.component("v-comments", VComments);
+    Vue.component("v-extensions-store", VExtensionsStore);
+    Vue.component("v-facets", VFacets);
+    Vue.component("v-geopoint-input", VGeopointInput);
+    Vue.component("v-handles", VHandles);
+    Vue.component("v-json-editor", VJsonEditor);
+    Vue.component("v-notifications", VNotifications);
+    Vue.component("v-map", VMap);
+    
+    // directives
+    Vue.directive("minify", VMinify);
+    Vue.directive("scroll-spy", VScrollSpy);
+
+    Vue.http.interceptors.push(function(/*request*/) {
+        return function(response) { if(!response.ok) { VMethods.onAjaxError.bind(this)(response); } };
+     });
+
+    if (Quasar.lang.enUs) {
+        Quasar.lang.enUs.vui = EnUs;
+    }
+    if (Quasar.lang.fr) {
+        Quasar.lang.fr.vui = Fr;
+    }
+    
+   
+}
+
+export let methods = VMethods
---
+++ vertigo-ui-vuejs/vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    configureWebpack: {
+    	output: {
+    	  library: 'VertigoUi', // Add this line to expose the library in the devServer
+    	  libraryTarget: 'umd'
+    	},
+	    devServer: {
+	      headers: { 'Access-Control-Allow-Origin': '*' }
+	    },
+        externals: {
+            quasar: {
+                commonjs: 'quasar',
+                commonjs2: 'quasar',
+                root: 'Quasar'
+            },
+            ol: {
+                commonjs: 'ol',
+                commonjs2: 'ol',
+                root: 'ol'
+            },
+            vue: {
+                commonjs: 'vue',
+                commonjs2: 'vue',
+                root: 'Vue'
+            }
+        }
+    }
+}
