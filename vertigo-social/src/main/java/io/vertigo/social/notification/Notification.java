75d14066a1facb61a4f3ee0ae246b0645d37fa17
phlppchrtn <philippe.chretien@kleegroup.com>
2021-10-01 19:58:04 +0200

    notification > record

--- vertigo-social/src/main/java/io/vertigo/social/notification/Notification.java
+++ vertigo-social/src/main/java/io/vertigo/social/notification/Notification.java
@@ -25,39 +25,28 @@ import io.vertigo.core.lang.Assertion;
 
 /**
  * @author pchretien, npiedeloup, btounkara
+ * @param uuid Uuid
+ * @param sender Sender name
+ * @param type Type
+ * @param title Title
+ * @param content Content
+ * @param ttlInSeconds TimeToLive in seconds
+ * @param creationDate Create date
+ * @param targetUrl Target URL of this notification
+ * @param userContentOpt Reader's specific content of this notification (can't be empty)
  */
-public final class Notification {
-	private final UUID uuid;
-	private final String sender;
-	private final String type;
-	private final String title;
-	private final String content;
-	private final int ttlInSeconds;
-	private final String targetUrl;
-	private final Instant creationDate;
-	private final Optional<String> userContentOpt;
+public record Notification(
+		UUID uuid,
+		String sender,
+		String type,
+		String title,
+		String content,
+		int ttlInSeconds,
+		Instant creationDate,
+		String targetUrl,
+		Optional<String> userContentOpt) {
 
-	/**
-	 * Constructor.
-	 * @param uuid Uuid
-	 * @param sender Sender name
-	 * @param type Type
-	 * @param title Title
-	 * @param content Content
-	 * @param ttlInSeconds TimeToLive in seconds
-	 * @param creationDate Create date
-	 * @param targetUrl Target URL of this notification
-	 * @param userContentOpt Reader's specific content of this notification (can't be empty)
-	 */
-	Notification(final UUID uuid,
-			final String sender,
-			final String type,
-			final String title,
-			final String content,
-			final int ttlInSeconds,
-			final Instant creationDate,
-			final String targetUrl,
-			final Optional<String> userContentOpt) {
+	public Notification {
 		Assertion.check()
 				.isNotNull(uuid)
 				.isNotBlank(sender)
@@ -70,16 +59,6 @@ public final class Notification {
 				.isNotNull(userContentOpt)
 				.when(userContentOpt.isPresent(), () -> Assertion.check()
 						.isTrue(userContentOpt.get().length() > 0, "userContent can't be empty if set"));
-		//-----
-		this.uuid = uuid;
-		this.sender = sender;
-		this.type = type;
-		this.title = title;
-		this.content = content;
-		this.ttlInSeconds = ttlInSeconds;
-		this.creationDate = creationDate;
-		this.targetUrl = targetUrl;
-		this.userContentOpt = userContentOpt;
 	}
 
 	/**
@@ -98,68 +77,4 @@ public final class Notification {
 	public static NotificationBuilder builder(final UUID uuid) {
 		return new NotificationBuilder(uuid);
 	}
-
-	/**
-	 * @return Uuid
-	 */
-	public UUID getUuid() {
-		return uuid;
-	}
-
-	/**
-	 * @return Sender's name
-	 */
-	public String getSender() {
-		return sender;
-	}
-
-	/**
-	 * @return Notification's type
-	 */
-	public String getType() {
-		return type;
-	}
-
-	/**
-	 * @return Notification's type
-	 */
-	public String getTitle() {
-		return title;
-	}
-
-	/**
-	 * @return Notification's content
-	 */
-	public String getContent() {
-		return content;
-	}
-
-	/**
-	 * @return Notification's TTL in seconds
-	 */
-	public int getTTLInSeconds() {
-		return ttlInSeconds;
-	}
-
-	/**
-	 * @return Creation date
-	 */
-	public Instant getCreationDate() {
-		return creationDate;
-	}
-
-	/**
-	 * @return Notification's target url
-	 */
-	public String getTargetUrl() {
-		return targetUrl;
-	}
-
-	/**
-	 * @return Specific content linked to reader
-	 */
-	public Optional<String> getUserContent() {
-		return userContentOpt;
-	}
-
 }
