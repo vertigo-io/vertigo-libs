3302ef6b9956da1b9b3389ab2c850ee33925cd82
phlppchrtn <philippe.chretien@kleegroup.com>
2021-10-06 12:47:15 +0200

    record

--- vertigo-datamodel/src/test/java/io/vertigo/datamodel/smarttype/data/domain/GeoPoint.java
+++ vertigo-datamodel/src/test/java/io/vertigo/datamodel/smarttype/data/domain/GeoPoint.java
@@ -17,22 +17,5 @@
  */
 package io.vertigo.datamodel.smarttype.data.domain;
 
-public class GeoPoint {
-
-	private final int x;
-	private final int y;
-
-	public GeoPoint(final int x, final int y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
+public record GeoPoint(int x, int y) {
 }
