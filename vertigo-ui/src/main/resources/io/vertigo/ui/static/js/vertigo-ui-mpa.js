927608a9702849f682907f78508d4c0d9b6831c4
Matthieu Laroche <matthieu.laroche@telecom-bretagne.eu>
2023-03-31 17:06:36 +0200

    [ui] support pendingActions and spinner css (delay)

--- vertigo-ui/src/main/resources/io/vertigo/ui/static/js/vertigo-ui-mpa.js
+++ vertigo-ui/src/main/resources/io/vertigo/ui/static/js/vertigo-ui-mpa.js
@@ -27,5 +27,12 @@ var VUiPage = new Vue({
 // fixes wrong components states due to firefox bfcache (back-forward cache)
 //https://developer.mozilla.org/en-US/docs/Archive/Misc_top_level/Working_with_BFCache
 window.addEventListener('pageshow', function(event) {
-	VUiPage.$forceUpdate();
+    VUiPage.$forceUpdate();    
+});
+
+window.addEventListener('pagehide', function(event) {
+    // on retire les pendingActions, pour le cas du back
+    if(VUiPage.componentStates && VUiPage.componentStates.pendingAction) {
+        VUiPage.componentStates.pendingAction.actionNames = [];
+    }  
 });
