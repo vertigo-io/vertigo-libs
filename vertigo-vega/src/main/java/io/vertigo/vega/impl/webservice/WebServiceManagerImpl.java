9d7ff9853a176ac6e531e12ce5e10620402497e5
phlppchrtn <philippe.chretien@kleegroup.com>
2023-05-31 17:30:42 +0200

    import

--- vertigo-vega/src/main/java/io/vertigo/vega/impl/webservice/WebServiceManagerImpl.java
+++ vertigo-vega/src/main/java/io/vertigo/vega/impl/webservice/WebServiceManagerImpl.java
@@ -18,7 +18,6 @@
 package io.vertigo.vega.impl.webservice;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
