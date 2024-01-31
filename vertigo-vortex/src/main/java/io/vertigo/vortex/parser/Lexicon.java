085523001b74228cd55291528cbd0d5bac4f8586
phlppchrtn <philippe.chretien@kleegroup.com>
2024-02-01 00:49:18 +0100

    [vortex] clean tokenizer

--- vertigo-vortex/src/main/java/io/vertigo/vortex/parser/Lexicon.java
+++ vertigo-vortex/src/main/java/io/vertigo/vortex/parser/Lexicon.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.vertigo.vortex.tokenizer;
+package io.vertigo.vortex.parser;
 
 /**
  * Contains the core elements of any structural grammar.
