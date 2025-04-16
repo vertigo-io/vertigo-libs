e395141c814e23886067fcd6a53efde1944a8194
ppinette <paul.pinette@kleegroup.com>
2025-04-16 15:54:41 +0200

    [dsfr-ui] better facets style

--- vertigo-ui-dsfr/src/components/DsfrFacets.vue
+++ vertigo-ui-dsfr/src/components/DsfrFacets.vue
@@ -190,13 +190,13 @@ export default {
 </script>
 
 <style scoped>
-.facets .fr-btn {
+.facets div[role=group] .fr-btn {
   min-width: 100%;
   padding: .25rem .8rem;
   min-height: 2rem;
 }
 
-.facets .fr-btn > :deep(span) {
+.facets div[role=group] .fr-btn > :deep(span) {
   min-width: 100%;
   color: var(--text-label-grey);
   font-weight: normal;
