4f67b049c65b0ad69d3a1e12c89b81f8a26d9635
phlppchrtn <philippe.chretien@kleegroup.com>
2026-05-22 19:36:09 +0200

    [vega] architecture en modules

---
+++ vertigo-vega/src/test/java/io/vertigo/vega/VegaArchitectureTest.java
@@ -0,0 +1,20 @@
+package io.vertigo.vega;
+
+import java.io.File;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+
+import io.vertigo.arch.ArchChecker;
+
+@AnalyzeClasses(packages = "io.vertigo.vega", importOptions = { ImportOption.DoNotIncludeTests.class })
+class VegaArchitectureTest {
+	private static final File YAML = new File("src/test/java/io/vertigo/vega/vega-modules.yaml");
+
+	@ArchTest
+	void module_dependency_rules(final JavaClasses classes) {
+		ArchChecker.check(classes, YAML);
+	}
+}
