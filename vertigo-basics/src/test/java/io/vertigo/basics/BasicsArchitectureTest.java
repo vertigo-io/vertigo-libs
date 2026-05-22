0f2788d8c83c0683ca8a2c46631e9059ec303bd2
phlppchrtn <philippe.chretien@kleegroup.com>
2026-05-22 19:35:17 +0200

    [basics] architecture en modules

---
+++ vertigo-basics/src/test/java/io/vertigo/basics/BasicsArchitectureTest.java
@@ -0,0 +1,20 @@
+package io.vertigo.basics;
+
+import java.io.File;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+
+import io.vertigo.arch.ArchChecker;
+
+@AnalyzeClasses(packages = "io.vertigo.basics", importOptions = { ImportOption.DoNotIncludeTests.class })
+class BasicsArchitectureTest {
+	private static final File YAML = new File("src/test/java/io/vertigo/basics/basics-modules.yaml");
+
+	@ArchTest
+	void module_dependency_rules(final JavaClasses classes) {
+		ArchChecker.check(classes, YAML);
+	}
+}
