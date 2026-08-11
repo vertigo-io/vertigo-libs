68efa11605238b809f1294f2799e2e84b2b2acde
skerdudou <skerdudou@users.noreply.github.com>
2026-08-11 18:08:24 +0200

    Fix javax.inject -> jakarta.inject in SpeedbKVStoreManagerTest (v4 merge adaptation)

--- vertigo-datastore/src/test/java/io/vertigo/datastore/kvstore/speedb/SpeedbKVStoreManagerTest.java
+++ vertigo-datastore/src/test/java/io/vertigo/datastore/kvstore/speedb/SpeedbKVStoreManagerTest.java
@@ -17,7 +17,7 @@
  */
 package io.vertigo.datastore.kvstore.speedb;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
